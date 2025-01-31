a04ae272868023fc45cdb9c97ec06269bf725cb8
ArikSquad <75741608+ArikSquad@users.noreply.github.com>
2025-01-31 18:07:12 +0200

    feat: Initial AbeoVanish support

--- paper/src/main/java/eu/mikart/animvanish/AnimVanishPaper.java
+++ paper/src/main/java/eu/mikart/animvanish/AnimVanishPaper.java
@@ -3,6 +3,7 @@ package eu.mikart.animvanish;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIBukkitConfig;
 import eu.mikart.animvanish.effects.impl.TntEffect;
+import eu.mikart.animvanish.hooks.impl.AbeoVanishHook;
 import lombok.NoArgsConstructor;
 import net.kyori.adventure.audience.Audience;
 import org.bukkit.entity.Player;
@@ -13,28 +14,29 @@ import java.util.UUID;
 @NoArgsConstructor
 public class AnimVanishPaper extends AnimVanishBukkit {
 
-	@Override
-	public void onLoad() {
-		CommandAPI.onLoad(new CommandAPIBukkitConfig(this).shouldHookPaperReload(true).silentLogs(true));
-	}
+    @Override
+    public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIBukkitConfig(this).shouldHookPaperReload(true).silentLogs(true));
+    }
 
-	@Override
-	@NotNull
-	public Audience getAudience(@NotNull UUID user) {
-		final Player player = getServer().getPlayer(user);
-		return player == null || !player.isOnline() ? Audience.empty() : player;
-	}
+    @Override
+    @NotNull
+    public Audience getAudience(@NotNull UUID user) {
+        final Player player = getServer().getPlayer(user);
+        return player == null || !player.isOnline() ? Audience.empty() : player;
+    }
 
-	@Override
-	public void loadPlatform() {
-		CommandAPI.onEnable();
-		areCommandsInitialized = true;
-		getEffectManager().registerEffect(new TntEffect(this));
-	}
+    @Override
+    public void loadPlatform() {
+        CommandAPI.onEnable();
+        areCommandsInitialized = true;
+        getEffectManager().registerEffect(new TntEffect(this));
+        getHooks().add(new AbeoVanishHook());
+    }
 
-	@Override
-	public void unloadPlatform() {
-		CommandAPI.onDisable();
-	}
+    @Override
+    public void unloadPlatform() {
+        CommandAPI.onDisable();
+    }
 
 }
---
+++ paper/src/main/java/eu/mikart/animvanish/hooks/impl/AbeoVanishHook.java
@@ -0,0 +1,29 @@
+package eu.mikart.animvanish.hooks.impl;
+
+import eu.mikart.abeovanish.api.BukkitAbeoVanishAPI;
+import eu.mikart.animvanish.hooks.Hook;
+import eu.mikart.animvanish.user.BukkitUser;
+import eu.mikart.animvanish.user.OnlineUser;
+import org.bukkit.entity.Player;
+
+public class AbeoVanishHook extends Hook {
+
+    private final BukkitAbeoVanishAPI abeoVanishAPI;
+
+    public AbeoVanishHook() {
+        super("AbeoVanish");
+        this.abeoVanishAPI = BukkitAbeoVanishAPI.getInstance();
+    }
+
+    @Override
+    public void vanish(OnlineUser p) {
+        Player player = ((BukkitUser) p).getPlayer();
+        abeoVanishAPI.setVanished(player, !isVanished(p));
+    }
+
+    @Override
+    public boolean isVanished(OnlineUser p) {
+        Player player = ((BukkitUser) p).getPlayer();
+        return abeoVanishAPI.isVanished(player);
+    }
+}
